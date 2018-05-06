53f4050b0cdd570084bebb7048221eb6ccbf608f
like3110 <328594663@qq.com>
2018-05-06 18:22:06 +0800

    pep8

--- bin/db_connect.py
+++ bin/db_connect.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#数据库常用连接服务
+# 数据库常用连接服务
 
 import base64
 from Crypto.Cipher import AES
 import cx_Oracle
 import pymysql
 from impala.dbapi import connect
+
 try:
     import xml.etree.cElementTree as etree
 except ImportError:
@@ -22,7 +23,8 @@ def decrypt(encryptedPassword):
     plainPassword = unpad(cipher.decrypt(base64Decoded[:-32]))[AES.block_size:]
     return plainPassword
 
-def get_db_config(config_file,DB_NAME):
+
+def get_db_config(config_file, DB_NAME):
     tree = etree.parse(config_file)
     elem = tree.find('auth[@DB_NAME="%s"]' % DB_NAME)
     v_db_type = elem.findtext('type')
@@ -34,16 +36,18 @@ def get_db_config(config_file,DB_NAME):
         v_db_sid = elem.findtext('sid')
         v_server_name = elem.findtext('server_name')
         if v_server_name is None or v_server_name == '':
-            v_db_cfg = ['0',v_db_sid]
+            v_db_cfg = ['0', v_db_sid]
         else:
-            v_db_cfg = ['1',v_server_name]
+            v_db_cfg = ['1', v_server_name]
     elif v_db_type == 'mysql':
         v_db_cfg = elem.findtext('db')
     else:
         print('Wrong db_type,please check!')
         exit(1)
-    db_config = {'db_type':v_db_type,'db_host':v_db_host,'db_port':v_db_port,'db_cfg':v_db_cfg,'db_username':v_db_username,'db_password':v_db_password}
+    db_config = {'db_type': v_db_type, 'db_host': v_db_host, 'db_port': v_db_port, 'db_cfg': v_db_cfg,
+                 'db_username': v_db_username, 'db_password': v_db_password}
     return db_config
+
 
 def get_connect(db_config):
     v_db_type = db_config['db_type']
@@ -51,7 +55,7 @@ def get_connect(db_config):
     v_db_port = db_config['db_port']
     v_db_username = db_config['db_username']
     v_db_password = decrypt(db_config['db_password'])
-    v_db_password = str(v_db_password,'utf-8')
+    v_db_password = str(v_db_password, 'utf-8')
     v_db_cfg = db_config['db_cfg']
     if v_db_type == 'oracle':
         if v_db_cfg[0] == '0':
@@ -61,18 +65,20 @@ def get_connect(db_config):
         connection = cx_Oracle.connect(v_db_username, v_db_password, dsn)
     elif v_db_type == 'mysql':
         v_db_port = int(v_db_port)
-        connection = pymysql.connect(host=v_db_host, port=v_db_port, user=v_db_username, passwd=v_db_password, db=v_db_cfg)
+        connection = pymysql.connect(host=v_db_host, port=v_db_port, user=v_db_username, passwd=v_db_password,
+                                     db=v_db_cfg)
     return connection
 
+
 if __name__ == '__main__':
-    #auth = get_db_config('db_config,xml','ORACLE189')
-    #print(auth)
-    #conn = get_connect(auth)
-    #cursor = conn.cursor()
-    #sql = "SELECT * from user_tables"
-    #cursor.execute(sql)
-    #result = cursor.fetchall()
-    #print(result)
-    conn = connect(host='10.45.28.209', port=10000, timeout=3600,user='root',password='')
+    # auth = get_db_config('db_config,xml','ORACLE189')
+    # print(auth)
+    # conn = get_connect(auth)
+    # cursor = conn.cursor()
+    # sql = "SELECT * from user_tables"
+    # cursor.execute(sql)
+    # result = cursor.fetchall()
+    # print(result)
+    conn = connect(host='10.45.28.209', port=10000, timeout=3600, user='root', password='')
     cur = conn.cursor()
-    #cur.execute('select name as num from user ;')
+    # cur.execute('select name as num from user ;')
